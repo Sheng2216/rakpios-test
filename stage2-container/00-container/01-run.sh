3c5c52884f800c2b842c6317d48a48f0832813d3
“sheng2216” <sheng.lyu@rakwireless.com>
2022-11-04 05:23:11 +0000

    fix error

--- stage2-container/00-container/01-run.sh
+++ stage2-container/00-container/01-run.sh
@@ -2,20 +2,24 @@
 
 # Create a directory structure to store run once scripts
 on_chroot << EOF
-mkdir -p "${ROOTFS_DIR}/etc/local/runonce.d/ran"
+mkdir -p /etc/local/runonce.d/ran
 EOF
 
 # copy portainer.sh.sample
 cp files/portainer.sh.sample "${ROOTFS_DIR}/etc/local/runonce.d"
+echo "1"
 # copy the runonce script
 cp files/runonce "${ROOTFS_DIR}/etc/local/bin/runonce"
+echo "2"
 # copy the rc.local script
 cp files/rc.local "${ROOTFS_DIR}/etc/rc.local"
-
+echo "3"
 
 # configure the portainer premission
 on_chroot << EOF
 chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} /etc/local/runonce.d/portainer.sh.sample
+echo "4"
 chmod +x /etc/local/runonce.d/portainer.sh.sample
+echo "5"
 EOF
 
